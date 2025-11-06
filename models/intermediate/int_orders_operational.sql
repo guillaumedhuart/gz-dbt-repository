952d61ad2086d5bfdae02384f9676bc66a0fd690
guillaume.dhuart@hotmail.fr <guillaume.dhuart@hotmail.fr>
2025-11-06 11:17:23 +0000

    log cost syntax

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -9,7 +9,7 @@
      ,o.purchase_cost
      ,o.margin
      ,s.shipping_fee
-     ,s.log_cost
+     ,s.logcost as log_cost
      ,s.ship_cost
  FROM {{ref("int_orders_margin")}} o
  LEFT JOIN {{ref("stg_raw__ship")}} s
